f62b4b4910b6b497fe1d11e4e7596b343482aa6a
joe0110 <2228698741@qq.com>
2019-08-29 20:21:42 +0800

    '环境搭建'

---
+++ ECMAScript/build/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.[hash].js'
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules:[
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin({}),
+    new HtmlWebpackPlugin({
+      template: 'src/template/index.html'
+    })
+  ],
+  devtool: process.env.NODE_ENV === 'production'? false : 'inline-sourcemap',
+  devServer: {
+    // contentBase: '../dist/',
+    stats: "errors-only",
+    compress: false,
+    host: 'localhost',
+    port: 8089
+  }
+}
